16ed4de8bc516d8d5ed724ceba3c292d9351d2b5
kchen8921 <chenkewei1989@gmail.com>
2020-04-08 03:55:31 -0700

    sandbox implementation

--- data/compile.sh
+++ data/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+rm -r /bin/pflotran/
 git clone https://bitbucket.org/pflotran/pflotran
-cp /kb/module/work/tmp/scratch/reaction_sandbox_pnnl_cyber.F90 /bin/pflotran/src/pflotran
+# cp /kb/module/work/tmp/scratch/reaction_sandbox_pnnl_cyber.F90 /bin/pflotran/src/pflotran
 cd /bin/pflotran/src/pflotran
 make pflotran
