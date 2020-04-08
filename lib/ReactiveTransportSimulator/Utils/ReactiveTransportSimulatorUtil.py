350b2cb3ecd6685b84599daadc50c1eadd4b2d22
kchen8921 <chenkewei1989@gmail.com>
2020-04-08 10:29:59 -0700

    run pflotran test

--- lib/ReactiveTransportSimulator/Utils/ReactiveTransportSimulatorUtil.py
+++ lib/ReactiveTransportSimulator/Utils/ReactiveTransportSimulatorUtil.py
@@ -49,8 +49,9 @@ class ReactiveTransportSimulatorRunBatchUtil:
         batch_deck = os.path.join(self.scratch_folder,'batch.in')
         db = os.path.join(self.scratch_folder,'database.dat')
         comps = os.path.join(self.data_folder,'temp_comps.tsv')
-        print('input_model_parameters:',self.params['input_model_parameters'])
-        init_cond = os.path.join(self.data_folder,'temp_cpd_initcond.csv')
+        dfu = DataFileUtil(self.callback_url)
+        init_cond = dfu.get_objects({'object_refs': [params['input_model_parameters']]})['data'][0]
+        # init_cond = os.path.join(self.data_folder,'temp_cpd_initcond.csv')
         # fba_data = self.dfu.get_objects({'object_refs': [fba]})['data'][0]
         # fba_obj = fba_data['data']
         # ?
