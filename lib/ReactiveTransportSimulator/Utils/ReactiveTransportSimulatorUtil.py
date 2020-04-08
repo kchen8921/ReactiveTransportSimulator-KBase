ba5bde5cdf4c9636a2eb6fb1b1ebe14eef860829
kchen8921 <chenkewei1989@gmail.com>
2020-04-08 02:05:22 -0700

    sandbox implementation

--- lib/ReactiveTransportSimulator/Utils/ReactiveTransportSimulatorUtil.py
+++ lib/ReactiveTransportSimulator/Utils/ReactiveTransportSimulatorUtil.py
@@ -89,7 +89,7 @@ class ReactiveTransportSimulatorRunBatchUtil:
         src_dir = '/bin/pflotran/src/pflotran'
         copy(sb_file,src_dir)
         compile_pflotran_cmd = "cd /bin/pflotran/src/pflotran && make fast=1 pflotran"
-        process = subprocess.Popen(compile_pflotran_cmd, stdout=subprocess.PIPE)
+        process = subprocess.Popen(compile_pflotran_cmd.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
         print("Compile PFLOTRAN output:",output[-300:])
         print("Complile PFLOTRAN err:",error)
