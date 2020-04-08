c31fdd98788b217e8de6ce3c161a6eb50c4a78b3
kchen8921 <chenkewei1989@gmail.com>
2020-04-08 08:38:01 -0700

    sandbox implementation

--- data/compile.sh
+++ data/compile.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# rm -r /bin/pflotran/
-# git clone https://bitbucket.org/pflotran/pflotran
-diff -c /kb/module/work/tmp/scratch/reaction_sandbox_pnnl_cyber.F90 /kb/module/data/reaction_sandbox_pnnl_cyber.F90
-echo "Compsrison done"
 cd /bin/pflotran/src/pflotran
-# make fast=1 pflotran
+make fast=1 pflotran
