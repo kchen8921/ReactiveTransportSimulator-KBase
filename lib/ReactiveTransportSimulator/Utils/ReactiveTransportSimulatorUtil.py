a034b88ac07bc29f57a6554cbf91ca51a3c1ec11
kchen8921 <chenkewei1989@gmail.com>
2020-04-08 08:51:37 -0700

    sandbox implementation

--- lib/ReactiveTransportSimulator/Utils/ReactiveTransportSimulatorUtil.py
+++ lib/ReactiveTransportSimulator/Utils/ReactiveTransportSimulatorUtil.py
@@ -86,10 +86,10 @@ class ReactiveTransportSimulatorRunBatchUtil:
         print("Sandbox file generated.")
 
         # copy sandbox file to src dir and recompile pflotran
-        src_dir = '/bin/pflotran/src/pflotran'
-        copy(sb_file,src_dir)
-        # sb_test = os.path.join(self.data_folder,'reaction_sandbox_pnnl_cyber.F90')
-        # copy(sb_test,src_dir)
+        # src_dir = '/bin/pflotran/src/pflotran'
+        # copy(sb_file,src_dir)
+        sb_test = os.path.join(self.data_folder,'reaction_sandbox_pnnl_cyber.F90')
+        copy(sb_test,src_dir)
         print(os.getcwd())
         compile_pflotran_cmd = 'sh ./data/compile.sh'
         process = subprocess.Popen(compile_pflotran_cmd.split(), stdout=subprocess.PIPE)
